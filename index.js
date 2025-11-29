88216a8360209d5e3038afb02effa4bfafb1cbf9
elyorcoder <elyorcoder@gmail.com>
2025-11-29 17:24:21 +0500

    done

--- index.js
+++ index.js
@@ -18,6 +18,51 @@ const elhobby2 = document.querySelector(".wrapper__profile-p3");
 const elbirth2 = document.querySelector(".wrapper__profile-p4");
 const elgender2 = document.querySelector(".wrapper__profile-p5");
 const elsave = document.querySelector(".wrapper__profile-form-buttons-save");
+const namech = /^[A-Za-z]{8,}$/;
+const agech = /^(?:[1-9]|[1-9][0-9]|120){4,}$/;
+const emailch = /^[A-Za-z][A-Za-z._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{3,}$/;
+const hobbych = /^[A-Za-z]{10,}$/;
+const datech = /^[0-9]{5,}$/;
+function toastify(){
+  
+}
+elname.addEventListener("input", () => {
+  if (namech.test(elname.value.trim())) {
+    elname.style.border = "5px solid red";
+  } else {
+    elname.style.border = "2px solid aqua";
+  }
+});
+elage.addEventListener("input", () => {
+  if (agech.test(elage.value.trim())) {
+    elage.style.border = "5px solid red";
+  } else if (elage.value.trim() >= 126) {
+    elage.style.border = "5px solid red";
+  } else {
+    elage.style.border = "2px solid aqua";
+  }
+});
+elemail.addEventListener("input", () => {
+  if (emailch.test(elemail.value.trim())) {
+    elemail.style.border = "5px solid red";
+  } else {
+    elemail.style.border = "2px solid aqua";
+  }
+});
+elhobby.addEventListener("input", () => {
+  if (hobbych.test(elhobby.value.trim())) {
+    elhobby.style.border = "5px solid red";
+  } else {
+    elhobby.style.border = "2px solid aqua";
+  }
+});
+elbirth.addEventListener("input", () => {
+  if (datech.test(elbirth.value.trim())) {
+    elbirth.style.border = "5px solid red";
+  } else {
+    elbirth.style.border = "2px solid aqua";
+  }
+});
 
 function bigfirst(a) {
   //   const b = a.trim();
@@ -33,23 +78,19 @@ function small(a) {
 }
 function number(a) {
   let b = a.value;
-  if(b > 0 & b <= 12){
+  if ((b > 0) & (b <= 12)) {
     b = "Child";
     return b;
-  }
-  else if(b > 12 & b <= 19){
+  } else if ((b > 12) & (b <= 19)) {
     b = "Teen";
     return b;
-  }
-  else if(b > 19 & b <= 59){
+  } else if ((b > 19) & (b <= 59)) {
     b = "Adult";
     return b;
-  }
-  else if(b > 59 & b <= 125){
+  } else if ((b > 59) & (b <= 125)) {
     b = "Senior";
     return b;
-  }
-  else{
+  } else {
     b = "Sen Haliyam Tirikmisan?";
     return b;
   }
@@ -91,13 +132,40 @@ elsave.addEventListener("click", () => {
   elage2.textContent = number(elage);
   elhobby2.textContent = repeat(elhobby);
   elbirth2.textContent = calculate(elbirth);
-  if(count){
-    elgender2.textContent="Male"
+  if (count) {
+    elgender2.textContent = "Male";
+  } else if (count2) {
+    elgender2.textContent = "Female";
+  } else {
+    alert("Chouse a gender");
   }
-  else if(count2){
-    elgender2.textContent="Female"
-  }
-  else{
-    alert("Chouse a gender")
-  }
+  Toastify({
+    text: "You are right",
+    duration: 1500,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+  Toastify({
+    text: "Yahooo",
+    duration: 1500,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+  
 });
